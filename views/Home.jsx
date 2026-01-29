39f0d007c94d14881a0d0cd8264d30bde1cd0cd8
doudaoud <boudaoudreda1@gmail.com>
2026-01-29 15:57:46 +0100

    rien de spiciale

--- views/Home.jsx
+++ views/Home.jsx
@@ -4,9 +4,20 @@ import Hero from './components/Hero'
 export default function Home() {
   return (
     <>
-      
       <Navbar />
-      <Hero/>
+      <Hero />
+      {/*debut de header 2  */}
+      <div className="header2" style={{
+        marginTop:"300px"
+      }}>
+        <div className="titles">
+          <h2>Powerful Features</h2>
+          <p>
+            Everything you need to manage your library efficiently and
+            effectively.
+          </p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
